b3713cb597d4b82f814c815435f6a3ff7efd05cd
srjranjan <sudhanshuwithyoualll@gmail.com>
2021-02-14 10:40:18 +0530

    ---

--- app/src/main/java/com/srj/waterlogger/MainActivity.java
+++ app/src/main/java/com/srj/waterlogger/MainActivity.java
@@ -1,16 +1,14 @@
 package com.srj.waterlogger;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-import com.srj.waterlogger.R;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_dashboard);
+        setContentView(R.layout.activity_sub_category);
     }
 }
