66e077c98a6795ca7a9a574d83fc8ab7a1f16e03
srjranjan <sudhanshuwithyoualll@gmail.com>
2021-02-19 21:56:53 +0530

    service layout created

--- app/src/main/java/com/srj/waterlogger/MainActivity.java
+++ app/src/main/java/com/srj/waterlogger/MainActivity.java
@@ -1,14 +1,22 @@
 package com.srj.waterlogger;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView, details;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sub_services);
+        setContentView(R.layout.service_selection);
+        textView = findViewById(R.id.service_details);
+        details = findViewById(R.id.details);
+        textView.setOnClickListener(v -> {
+            details.setVisibility(View.VISIBLE);
+        });
     }
 }
